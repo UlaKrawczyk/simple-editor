42187b71c9481f0c05f031e867fe45c4b6fa04d6
Ula Krawczyk <ula.krawczyk7@gmail.com>
2019-09-22 11:59:30 +0200

    feat: adding js logic

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,11 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, function (err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
@@ -16,7 +16,16 @@ if ('serviceWorker' in navigator) {
 
 // place your code below
 
+const textArea = document.querySelector('.editor__area--js');
+const btnLoad = document.querySelector('.editor__button--load');
+const btnSave = document.querySelector('.editor__button--save');
 
-console.log(`Hello world!`);
+btnSave.addEventListener('click', function () {
+  localStorage.setItem('areaInput', textArea.value);
+})
 
-
+btnLoad.addEventListener('click', function () {
+  if (localStorage.getItem('areaInput')) {
+    textArea.value = localStorage.getItem('areaInput');
+  }
+})
