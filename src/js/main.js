940b31428ea3ef1a8f3f99f70d07a3d99a260dc1
Ula Krawczyk <ula.krawczyk7@gmail.com>
2019-09-22 16:24:46 +0200

    feat: changing layout

--- src/js/main.js
+++ src/js/main.js
@@ -16,16 +16,14 @@ if ('serviceWorker' in navigator) {
 
 // place your code below
 
-const textArea = document.querySelector('.editor__area--js');
-const btnLoad = document.querySelector('.editor__button--load');
-const btnSave = document.querySelector('.editor__button--save');
+const textArea = document.querySelector('.form__area--js');
+const btnLoad = document.querySelector('.form__button--load');
+const btnSave = document.querySelector('.form__button--save');
 
 btnSave.addEventListener('click', function () {
   localStorage.setItem('areaInput', textArea.value);
 })
 
 btnLoad.addEventListener('click', function () {
-  if (localStorage.getItem('areaInput')) {
-    textArea.value = localStorage.getItem('areaInput');
-  }
+  textArea.value = localStorage.getItem('areaInput');
 })
